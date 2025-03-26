3d69b10d6a38c7131e3eec161b427228476e8f25
Ali Malekpour <542968+malekpour@users.noreply.github.com>
2025-03-26 18:41:54 -0400

    add flake.lock and update flake.nix for cross-compilation support and additional development tools

--- flake.nix
+++ flake.nix
@@ -4,24 +4,36 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in
-      {
+
+        crossPkgs = pkgs.pkgsCross.aarch64-multiplatform;
+      in {
         devShell = pkgs.mkShellNoCC {
           buildInputs = with pkgs; [
-            git-repo
+            # tools
+            bison
+            flex
+            swig
             gcc
+            openssl
+            gnutls
+            dtc
+            bc
+
+            # python
+            python3
+            python3.pkgs.setuptools
+            python3.pkgs.pyelftools
+            python3.pkgs.cryptography
+
+            # AArch64 cross toolchain (binaries: aarch64-unknown-linux-gnu-gcc, etc.)
+            crossPkgs.buildPackages.binutils
+            crossPkgs.buildPackages.gcc
           ];
 
           shellHook = ''
